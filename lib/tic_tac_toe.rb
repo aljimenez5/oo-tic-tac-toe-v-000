48e6a121dc571f6f4ac0baac0203c9501bf65d7a
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-04 01:10:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,7 +50,7 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
     end 
   end
 
-  def turn_count(@baord)
+  def turn_count(@board)
     counter = @board.count("X") + @board.count("O")
     return counter
   end
