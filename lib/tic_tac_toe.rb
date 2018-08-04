fc1a83c216a95fcddc20c7bc600295d96e939d31
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-04 00:57:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,8 +86,8 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
     end
   end
   
-  def over?(board)
-    if won?(board) || draw?(board) || full?(board) 
+  def over?
+    if won?(@board) || draw?(@board) || full?(@board) 
       return true
     else
       return false
