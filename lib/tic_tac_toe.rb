9dafa47fc8aa502308a6233bc3da25196c4ac52c
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-04 00:56:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,7 +75,7 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
   end
   
   def full?
-    board.none? { |empty_index| empty_index == " "}
+    @board.none? { |empty_index| empty_index == " "}
   end
   
   def draw?(board)
