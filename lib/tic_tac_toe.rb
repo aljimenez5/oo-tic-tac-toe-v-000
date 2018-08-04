a10b1186ba13eb06a13d4a48678a22a0f4fc95f4
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-04 00:56:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,7 +79,7 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
   end
   
   def draw?
-    if full?(@board) && !won?(b
+    if full?(@board) && !won?(@board)
       return true
     else
       return false
