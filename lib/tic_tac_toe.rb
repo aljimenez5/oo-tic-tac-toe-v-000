c47e721790a5ad9c75325bd4a5d7edf060dde96e
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-04 01:26:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,7 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
     @board[user_input] = token
   end
   
-  def position_taken?(board, user_input)
+  def position_taken?(user_input)
     if @board[user_input] == "X" || @board[user_input] == "O"
       true
     else
