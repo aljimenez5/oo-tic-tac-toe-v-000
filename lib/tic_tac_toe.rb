3305b8c47e004238bf60d703c135d55d86440fbf
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-04 01:14:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,16 +15,16 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
   end
   
   def input_to_index(user_input)
-    @user_input = user_input.to_i - 1
+    user_input = user_input.to_i - 1
     return @user_input
   end
   
   def move(user_input, token="X")
-    @board[@user_input] = token
+    @board[user_input] = token
   end
   
   def position_taken?(board, user_input)
-    if @board[@user_input] == "X" || @board[@user_input] == "O"
+    if @board[user_input] == "X" || @board[user_input] == "O"
       true
     else
       false
