2f471a9ca48c2090d8c3dfba73cf9432c754368e
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-04 00:56:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,7 +65,7 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
     end
   end
 
-  def won?(board)
+  def won?
     for combination in WIN_COMBINATIONS do
       if @board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X" || @board[combination[0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O" 
         return combination
