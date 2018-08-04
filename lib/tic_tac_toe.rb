0236ed4d75ab43838d7f173aca84bc9b5124ad1f
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-04 01:13:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,7 +20,7 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
   end
   
   def move(user_input, token="X")
-    @board[user_input] = token
+    @board[@user_input] = token
   end
   
   def position_taken?(board, user_input)
