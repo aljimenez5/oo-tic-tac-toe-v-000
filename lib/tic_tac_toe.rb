a9ebc1ad4a922f9c63d3961091e463aa78b551e7
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-04 00:48:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,9 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
   end
   
   def input_to_index
-    
+    user_input = user_input.to_i - 1
+    return user_input
+  end
   
   
   
